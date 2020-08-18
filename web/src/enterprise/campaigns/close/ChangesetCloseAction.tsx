2cdc806e21394964ed4fda7772cf40d68bba570e
Erik Seliger <erikseliger@me.com>
2020-08-18 03:30:57 +0200

    Cleanup sass

--- web/src/enterprise/campaigns/close/ChangesetCloseAction.tsx
+++ web/src/enterprise/campaigns/close/ChangesetCloseAction.tsx
@@ -6,7 +6,7 @@ import classNames from 'classnames'
 const iconClassNames = 'm-0 mx-4 text-nowrap d-flex flex-column align-items-center justify-content-center'
 
 export const ChangesetCloseActionClose: React.FunctionComponent<{}> = () => (
-    <div className={classNames(iconClassNames, 'will-close')}>
+    <div className={classNames(iconClassNames, 'changeset-close-action__close-flash')}>
         <CloseCircleOutlineIcon data-tooltip="This changeset will be closed on the code host when the campaign is closed." />
         <span className="text-muted">Will close</span>
     </div>
